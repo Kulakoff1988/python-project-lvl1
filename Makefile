6fd3ca664267041bf1e85ce748a96061a52994bb
Aleksandr Kulakov <kulakofft4@gmail.com>
2019-06-28 17:42:19 +0300

    first version of third step
    add yml-file and set options, try to fix code with flake8

--- Makefile
+++ Makefile
@@ -1,2 +1,5 @@
+all: install
 install:
 	@python3 -m poetry install
+lint:
+	@python3 -m poetry run flake8 brain_games
--- brain_games/cli.py
+++ brain_games/cli.py
@@ -3,4 +3,3 @@ import prompt
 def run():
 	name = prompt.string('Welcome to the Brain Games!\n\nMay I have your name? ')
 	print('Hello, {}!'.format(name))
-
