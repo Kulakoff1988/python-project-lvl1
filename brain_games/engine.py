3c82b11e19add3e98d8ee762dbc47d56ad6dec86
Aleksandr Kulakov <kulakofft4@gmail.com>
2019-07-06 18:02:24 +0300

    some fixes

--- brain_games/engine.py
+++ brain_games/engine.py
@@ -1,24 +1,24 @@
 from brain_games.cli import player_greeting, get_player_name, get_player_answer
 
 
-def run(game_description, game_data):
-    print_game_title(game_description)
+def run(game_title, game_data):
+    print_game_title(game_title)
     PLAYER_NAME = get_player_name()
 
     print_game_process(PLAYER_NAME, game_data)
 
 
-def print_game_title(game_description):
+def print_game_title(game_title):
     player_greeting()
-    print(f'{game_description}\n\n')
+    print(f'{game_title}\n\n')
 
 
 def print_game_process(PLAYER_NAME, game_data):
     correct_answers = 0
     while correct_answers < 3:
         GAME_DATA = game_data()
-        question = GAME_DATA['question']
-        print(f'Question: {question}')
+        QUESTION = GAME_DATA['question']
+        print(f'Question: {QUESTION}')
         RIGHT_ANSWER = GAME_DATA['answer']
         PLAYER_ANSWER = get_player_answer()
         if PLAYER_ANSWER == RIGHT_ANSWER:
